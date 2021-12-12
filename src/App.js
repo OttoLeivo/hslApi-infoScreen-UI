1064c62a34b56564562558324f2098de1952429d
Otto Leivo <0tt0.l31v0@gmail.com>
2021-12-12 18:53:00 +0200

    init + api working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const L_QUERY2 = `{
+  stops(name: "Herttoniemenranta") {
+    name
+    lat
+    id
+    lon
+    
+    wheelchairBoarding
+  }
+}`
 
 function App() {
+ React.useEffect(() => {
+fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({query : L_QUERY2})
+}).then(response => response.json())
+.then(data => console.log(data))
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +38,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>    
+           {
+           }
+         
       </header>
     </div>
   );
